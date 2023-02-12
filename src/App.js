2528e418fdfa4133d347e170d1469c1e8a370928
Faiz Murtadha <faizmrtdha99@gmail.com>
2023-02-12 14:21:58 +0700

    add: render navbar and homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Homepage from "components/home";
+import Navbar from "components/navbar";
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        <Homepage />
+      </main>
+    </div>
+  );
 };
 
 export default App;
